96c5bdd8cd40c82b4dab3e52c77cf4d4dac4eae4
Harvy00 <91102009+Harvy00@users.noreply.github.com>
2026-03-13 16:15:10 -0600

    Add database configuration settings in config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+Setting('DB_TYPE', 'pgsql');
+Setting('DB_HOST', 'db.uxsipuiwsglmmoqdljna.supabase.co');
+Setting('DB_NAME', 'postgres');
+Setting('DB_USER', 'postgres');
+Setting('DB_PASSWORD', 'Subninhar00*');
+Setting('DB_PORT', '5432');
